af1c68e67b8e7341e217f5849020976eb0c4a385
NikkiGetsee <Nicola.getsee@gmail.com>
2024-06-27 20:24:09 +0000

    created tests to check if keys exist in game

---
+++ scripts/game.js
@@ -0,0 +1,10 @@
+let game = {
+    score:0,
+    currentGame:[], //array
+    playerMoves:[], //array
+    choices:[] //array
+
+}
+
+module.exports = {game} 
+// we will be exporting more than one object so they need to go in curley braces, we need to import this in our game.test.js also, its the first line
---
+++ scripts/tests/game.test.js
@@ -0,0 +1,37 @@
+
+// this beforeAll code is the same for every html file that we want to load into the DOM we just might want to change file name
+
+const {game} = require("../game")
+
+beforeAll(()=> {
+let fs = require("fs")
+
+let fileContents = fs.readFileSync("index.html","utf-8");
+document.open()
+document.write(fileContents)
+document.close()
+})
+
+describe("game object contains correct keys", ()=> {
+    test("score key exists",() => {
+        // we are testing if the game object contains a key called score
+        expect("score" in game).toBe(true)
+    })
+
+
+    test("currentGame key exists",() => {
+        // we are testing if the game object contains a key called currentGame
+        expect("currentGame" in game).toBe(true)
+    })
+
+    test("playerMoves key exists",() => {
+        // we are testing if the game object contains a key called playerMoves
+        expect("playerMoves" in game).toBe(true)
+    })
+
+    test("choices key exists",() => {
+        // we are testing if the game object contains a key called choices
+        expect("choices" in game).toBe(true)
+    })
+})
+
