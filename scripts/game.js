7f56430e1e2d70fe9bd81f3207c081edc8543a32
NikkiGetsee <Nicola.getsee@gmail.com>
2024-06-29 18:25:43 +0000

    add data listener and lights on

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,8 @@ let game = {
     score:0,
     currentGame:[], //array
     playerMoves:[], //array
-    choices:["button1","button2","button3","button4"] //array
+    choices:["button1","button2","button3","button4"], //array
+    turnNumber : 0
 
 }
 
@@ -10,6 +11,17 @@ function newGame() {
     game.score = 0;
     game.playerMoves = [];
     game.currentGame = [];
+    for (let circle of document.getElementsByClassName("circle")){
+        if (circle.getAttribute("data-listener")!== "true") {
+            circle.addEventListener("click",(e) => {
+                let move = e.target.getAttribute("id")
+                lightsOn(move)
+                game.playerMoves.push(move)
+                playerTurn()
+            })
+            circle.setAttribute("data-listener","true")
+    }
+}
     showScore();
     addTurn();
 }
@@ -17,20 +29,18 @@ function addTurn() {
    
     game.playerMoves = []
     game.currentGame.push(game.choices[(Math.floor(Math.random() *4))])
+    showTurns()
 
     /**push onto  the computer game sequence,  
     we're going to go to our game.choices  key, which of course contains our four values,
     the IDs of our buttons. 
     
     And then  we're going to use the math.random library to generate a random number between zero and  three. We're going to use that as the index of our choices array and then the resulting  choice is pushed onto the current game array. */
-    //showTurns:
+    
+   
 }
 
 /**The showTurns function and player clicks should  cause the circle to change colour or to light up.  */
-function showTurns(){
-
-}
-
 
 
 function showScore() {
@@ -39,14 +49,28 @@ function showScore() {
 
 function lightsOn(circ){
     document.getElementById(circ).classList.add("light")
-    setTimeout(()=>{
-        document.getElementById(circ).classList.remove("light"),400
-    })
-    
-
+    setTimeout(function(){
+        document.getElementById(circ).classList.remove("light")},400
+    )
 }
 
-module.exports = {game,newGame,showScore,addTurn,lightsOn} 
+function showTurns(){
+    game.turnNumber = 0
+    let turns = setInterval(function() {
+        lightsOn(game.currentGame[game.turnNumber])
+        game.turnNumber++
+        if(game.turnNumber>=game.currentGame.length){
+            clearInterval(turns)
+        }
+    },800)
+}
+    /** it's setting this  interval turning the lightsOn,  
+    incrementing the game turnNumber,  and then turning them off again.
+ */
+
+
+
+module.exports = {game,newGame,showScore,addTurn,lightsOn,showTurns} 
 
 // we will be exporting more than one object so they need to go in curley braces, we need to import this in our game.test.js also, its the first line
 
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,7 +1,7 @@
 
 // this beforeAll code is the same for every html file that we want to load into the DOM we just might want to change file name
 
-const {game,newGame,showScore,addTurn,lightsOn} = require("../game")
+const {game,newGame,showScore,addTurn,lightsOn,showTurns} = require("../game")
 
 
 beforeAll(()=> {
@@ -37,6 +37,10 @@ describe("game object contains correct keys", ()=> {
     test("choices contains correct ids",()=>{
         expect(game.choices).toEqual(["button1","button2","button3","button4"]) //array to have the values of button1,button2...
     })
+
+    test("turnNumber Key exists", () => {
+        expect ("turnNumber" in game).toBe(true)
+    })
 })
 
 describe("newGame works correctly",()=>{
@@ -67,10 +71,15 @@ describe("newGame works correctly",()=>{
         expect(game.playerMoves.length).toBe(0)
         });
 
-    test("should display 0 for the element with the id of score ",()=>{
-    expect(document.getElementById("score").innerText).toEqual(0)})
+    test("expect data-listener to be true",()=>{
+    const elements = document.getElementsByClassName("circle")
+    for (let element of elements){
+        expect(element.getAttribute("data-listener")).toEqual("true")
+    }
+})
+    });
 
-    })
+        
 
 describe("gameplay works correctly",()=>{
     beforeEach(()=>{
@@ -94,6 +103,12 @@ describe("gameplay works correctly",()=>{
         let button = document.getElementById(game.currentGame[0]);
         lightsOn(game.currentGame[0])//get the element at index 0
         expect(button.classList).toContain("light")
+    })
+
+    test("showTurns should update the game.turnNumber",()=>{
+            game.turnNumber = 42
+            showTurns()
+            expect(game.turnNumber).toBe(0)
     })
 })
 
